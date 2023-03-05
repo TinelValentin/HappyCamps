78c217fb9bb173f675ca751c7dfbcd0dd039d39d
Tinel Valentin CARMACIU <tinel.carmaciu@student.unitbv.ro>
2023-03-05 20:52:49 +0200

    HAP-2-Implemented the register form

--- HappyCamps/src/app/app.module.ts
+++ HappyCamps/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { HttpClientModule } from '@angular/common/http';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+
+import { UserOutline, LockOutline, MailOutline, InstagramOutline, PhoneOutline } from '@ant-design/icons-angular/icons';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { MainComponent } from './main/main.component';
+
+const icons: IconDefinition[] = [UserOutline, LockOutline, MailOutline, InstagramOutline, PhoneOutline];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterFormComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    BrowserAnimationsModule,
+    NzIconModule.forRoot(icons),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ HappyCamps/src/app/register-form/register-form.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss']
+})
+export class RegisterFormComponent {
+
+  validateForm!: UntypedFormGroup;
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  constructor(private fb: UntypedFormBuilder) { }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required, Validators.maxLength(30)]],
+      password: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(30)]],
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      phone: [null, [Validators.required, Validators.pattern("^[0-9]*$"), Validators.maxLength(10), Validators.minLength(10)]],
+      instagram: [null, [Validators.required, Validators.pattern("^@.*$")]]
+    });
+  }
+
+}
